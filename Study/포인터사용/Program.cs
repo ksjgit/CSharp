fe2f8e4edb628068dbb5aac4df7d0d43efafddb7
ksjgit <ksjgit9000@gmail.com>
2017-09-11 00:12:30 +0900

    포인터사용

---
+++ Study/포인터사용/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace 포인터사용
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            for (int j = 0; j < 10000; j++)
+            {
+                //배열사용
+                //var ar = new byte[100000];
+                //ar[ar.Length - 1] = (byte)(j + 1);
+
+                //for (int i = 0; i < ar.Length; i++)
+                //{
+                //    if (ar[i] != 0) Console.WriteLine("Found number is {0}", ar[i]);
+                //}
+
+                //포인터 사용 <-- 프로젝트 속성에서 어셈블리 코드실행 허용해야함.
+                unsafe
+                {
+                    var ar = new byte[100000];
+                    ar[ar.Length - 1] = (byte)(j + 1);
+
+                    fixed (byte* ps = ar)
+                    {
+                        byte* p = ps;
+                        for (int i = 0; i < ar.Length; i++)
+                        {
+                            if(*p != 0) Console.WriteLine("Found number is {0}", *p);
+                            p++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(DateTime.Now - start);
+            Console.ReadLine();
+        }
+    }
+}
