cf0f8679cf93c65c8a82945919318129a49ffabd
ksjgit <ksjgit9000@gmail.com>
2017-09-10 23:41:43 +0900

    형변환, LINQ문법

---
+++ Study/형변환/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace 형변환
+{
+    class Base
+    {
+    }
+
+    class Extended : Base
+    {
+        public void SayIt()
+        {
+            Console.WriteLine("I am Extended!");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base[] array = { new Base(), new Extended()};
+
+
+            //1차
+            //foreach (var item in array)
+            //{
+            //    if (item is Extended)
+            //    {
+            //        ((Extended)item).SayIt();
+            //        Console.ReadLine();
+            //    }
+            //}
+
+            //2차
+            //foreach (var item in array)
+            //{
+            //    var extended = item as Extended;
+            //    if (extended != null)
+            //    {
+            //        extended.SayIt();
+            //        Console.ReadLine();
+            //    }
+            //}
+
+            foreach (var item in array.OfType<Extended>())
+            {
+                item.SayIt();
+                Console.ReadLine();
+            }
+        }
+    }
+}
