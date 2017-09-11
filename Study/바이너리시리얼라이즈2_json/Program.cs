5138a8ad8a8aac75c20bbdad187c2f8236fb14b5
ksj <ksj@idis.co.kr>
2017-09-11 20:18:50 +0900

    바이너리시리얼라이즈2_json

---
+++ Study/바이너리시리얼라이즈2_json/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+
+namespace 바이너리시리얼라이즈2_json
+{
+    [DataContract]
+    public class SampleSerializableClass
+    {
+        [DataMember()]
+        public string Value { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleSerializableClass obj = new SampleSerializableClass();
+            obj.Value = "Hello";
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(SampleSerializableClass));
+
+            using (Stream stream = new FileStream("sample.json", FileMode.Create))
+            {
+                serializer.WriteObject(stream, obj);
+            }
+            using (Stream stream = new FileStream("sample.json", FileMode.Open))
+            {
+                SampleSerializableClass read = (SampleSerializableClass)serializer.ReadObject(stream);
+                Console.WriteLine(read.Value);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
