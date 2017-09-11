3f17c1696c0f59abe714e8c6110ae41b79302cb9
ksj <ksj@idis.co.kr>
2017-09-11 19:04:37 +0900

    생성자_Create

---
+++ Study/생성자_Create/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 생성자_Create
+{
+    class Animal
+    {
+        public static Animal Create()
+        {
+            return new Animal();
+        }
+        public void SitDown()
+        {
+            Console.WriteLine("멍멍!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = Animal.Create();
+            dog.SitDown();
+
+            Console.ReadLine();
+        }
+    }
+}
