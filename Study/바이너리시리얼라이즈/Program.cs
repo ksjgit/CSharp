678b0716dae949d19a81675ab3b798051c95fd8d
ksj <ksj@idis.co.kr>
2017-09-11 20:10:15 +0900

    바이너리시리얼라이즈#1

---
+++ Study/바이너리시리얼라이즈/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace 바이너리시리얼라이즈
+{
+    [Serializable]
+    public class SampleSerializableClass
+    {
+        public string Value { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleSerializableClass obj = new SampleSerializableClass();
+            obj.Value = "Hello";
+            IFormatter formatter = new BinaryFormatter();
+
+            using (Stream stream = new FileStream("sample.bin", FileMode.Create))
+            {
+                formatter.Serialize(stream, obj);
+            }
+            using (Stream stream = new FileStream("sample.bin", FileMode.Open))
+            {
+                SampleSerializableClass read = (SampleSerializableClass)formatter.Deserialize(stream);
+                Console.WriteLine(read.Value);
+
+                Console.ReadLine();
+            }
+         }
+    }
+}
