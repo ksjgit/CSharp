04c16dfc8737d73d67ddb128c41d588a2c7058e2
ksj <ksj@idis.co.kr>
2017-09-11 19:49:06 +0900

    LINQtoXML

---
+++ Study/LINQtoXML/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace LINQtoXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "<root><a/><a/><a/><b/><a/><c/></root>";
+            var doc = XDocument.Parse(s);
+            var elements = doc.Descendants("a");
+
+            Console.WriteLine("a요소는 {0}개 입니다.", elements.Count());
+            Console.ReadLine();
+        }
+    }
+}
