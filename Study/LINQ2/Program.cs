e1a36c914e90e6a008d4821e52ff464b896192b4
ksj <ksj@idis.co.kr>
2017-09-11 21:03:02 +0900

    LINQ2

---
+++ Study/LINQ2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 5, 2, -1, 1, 4 };
+            int[] arWithoutMinus = ar.Where(c => c >= 0).ToArray();
+
+            //Console.WriteLine("결과 : {0}", arWithoutMinus.Sum());
+
+            Console.WriteLine("결과 : {0}", arWithoutMinus.Select((n, i) => new Tuple<int, int>(n, i)).Aggregate((sum, next) =>
+               {
+                   var r = sum.Item1 + next.Item1;
+                   Console.WriteLine("{0}번째 처리. 현재 합계 {1}", next.Item2, r);
+                   return new Tuple<int, int>(r, next.Item2);
+               }).Item1);
+            
+            Console.ReadLine();
+        }
+    }
+}
