48ab23d2d67c825ca66207e5c700092569eec841
ksj <ksj@idis.co.kr>
2017-09-11 19:29:21 +0900

    컬렉션반환_IEumerable#1

---
+++ Study/컬렉션반환_IEumerable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace 컬렉션반환_IEumerable
+{
+    class Program
+    {
+        private static List<int> searchItems(Func<int, bool> checkCondition)
+        {
+            int[] array = { 1, 2, 3 };
+            var list = new List<int>();
+
+            foreach (var item in array)
+            {
+                if (checkCondition(item)) list.Add(item);
+            }
+
+            return list;
+        }
+        static void Main(string[] args)
+        {
+            var r = searchItems((n) => n >= 2);
+            r.RemoveAt(1);
+            Console.WriteLine("결과는 {0}개 입니다.", r.Count);
+            Console.ReadLine();
+        }
+    }
+}
