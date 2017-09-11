4741c844e017ec4376b0c75873aa87a59bb2b826
ksj <ksj@idis.co.kr>
2017-09-11 19:41:00 +0900

    WinAPI호출

---
+++ Study/WinAPI호출/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinAPI호출
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        extern static bool Beep(uint dwFreq, uint dwDuration);
+        static void Main(string[] args)
+        {
+            Beep(1000, 1000);
+        }
+    }
+}
