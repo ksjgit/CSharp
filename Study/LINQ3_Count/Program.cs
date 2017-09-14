14ffffdcf93b3c61e096eb06427ba8586516b9ac
ksj <ksj@idis.co.kr>
2017-09-14 14:17:16 +0900

    LINQ3_Count

---
+++ Study/LINQ3_Count/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ3_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] winners = { 2, 1, 5, 3, 5 };
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine("등번호 {0}번", i);
+                if (winners.Contains(i))
+                    Console.WriteLine("우승했기 때문에 상금 {0}원 수여", winners.Count(c => c == i) * 1000000);
+                else
+                    Console.WriteLine("상금 없음");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
