05e5cd0b7ace87383346aeb2aa06f548e47f4f1b
ksj <ksj@idis.co.kr>
2017-09-14 14:00:33 +0900

    ToArray

---
+++ Study/ToArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            //var ar = Enumerable.Range(0, 100000);
+            var ar = Enumerable.Range(0, 100000).ToArray();
+            int sum = 0;
+            for (int i = 0; i < ar.Count(); i++)
+            {
+                sum = sum / 2 + ar.ElementAt(i);
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(DateTime.Now - start);
+
+            Console.ReadLine();
+        }
+    }
+}
