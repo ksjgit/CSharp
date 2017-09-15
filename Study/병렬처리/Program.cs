34f599227f57af004ed929c1e9413d7452ee3c3f
ksj <ksj@idis.co.kr>
2017-09-15 13:40:39 +0900

    병렬처리4 - ar.AsParallel().ForAll  (LINQ 사용) 다수쿼리 한꺼번에 날릴때 좋겠네..

--- Study/병렬처리/Program.cs
+++ Study/병렬처리/Program.cs
@@ -1,40 +1,14 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Linq;
 
 namespace 병렬처리
 {
     class Program
     {
-        private static void work(int n, int wait)
-        {
-            Task.Delay(1000 * wait).Wait();
-            Console.WriteLine("Task{0} Done", n);
-        }
         static void Main(string[] args)
         {
-            //var t1 = Task.Run(() => { work(1, 2); });
-            //var t2 = Task.Run(() => { work(2, 3); });
-            //var t3 = Task.Run(() => { work(3, 5); });
-            //var t4 = Task.Run(() => { work(4, 1); });
-
-            //for(;;)
-            //{
-            //    if (t1.IsCompleted && t2.IsCompleted && t3.IsCompleted && t4.IsCompleted) break;
-            //    Task.Delay(100).Wait();
-            //    Console.WriteLine("Waiting...");
-            //}
-
-            //Task.WaitAny(t1);
-            //Task.WaitAll(t1, t2, t3, t4);
-
-            Parallel.Invoke(
-                () => { work(1, 2); },
-                () => { work(2, 3); },
-                () => { work(3, 5); },
-                () => { work(4, 1); }
-                );
-
-            Console.WriteLine("All Done");
+            string[] ar = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            ar.AsParallel().ForAll(c => { Console.WriteLine(c); });
             Console.ReadLine();
         }
     }
