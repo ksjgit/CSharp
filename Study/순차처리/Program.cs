31ea19f39f6eb901197a4e800887863ae823dc75
ksj <ksj@idis.co.kr>
2017-09-15 12:45:14 +0900

    순차처리3 - 동기객체 사용(AutoResetEvent)

--- Study/순차처리/Program.cs
+++ Study/순차처리/Program.cs
@@ -9,9 +9,11 @@ namespace 순차처리
     {
         static void Main(string[] args)
         {
+            var evt = new AutoResetEvent(false);
             File.Delete("sample.text");
-            Task.Delay(1000).ContinueWith((dummy) =>
+            Task.Run(() =>
             {
+                evt.WaitOne(); //WaitHandle 이 신호를 받을때까지 현재 스레드 차단
                 try
                 {
                     Console.WriteLine(File.ReadAllText("sample.text"));
@@ -23,6 +25,7 @@ namespace 순차처리
             });
             Task.Delay(500).Wait();
             File.WriteAllText("sample.text", "Hello!");
+            evt.Set();
 
             Console.WriteLine("엔터키를 누르면 종료합니다.");
             Console.ReadLine();
