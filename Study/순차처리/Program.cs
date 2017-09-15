7926bfa0dd58a04edc0487e2f59e0b03c82f0eac
ksj <ksj@idis.co.kr>
2017-09-15 12:49:43 +0900

    순차처리 - lock 사용

--- Study/순차처리/Program.cs
+++ Study/순차처리/Program.cs
@@ -9,23 +9,27 @@ namespace 순차처리
     {
         static void Main(string[] args)
         {
-            var evt = new AutoResetEvent(false);
-            File.Delete("sample.text");
-            Task.Run(() =>
+            object obj = new object();
+            lock (obj)
             {
-                evt.WaitOne(); //WaitHandle 이 신호를 받을때까지 현재 스레드 차단
-                try
+                File.Delete("sample.text");
+                Task.Run(() =>
                 {
-                    Console.WriteLine(File.ReadAllText("sample.text"));
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            });
-            Task.Delay(500).Wait();
-            File.WriteAllText("sample.text", "Hello!");
-            evt.Set();
+                    try
+                    {
+                        lock (obj)
+                        {
+                            Console.WriteLine(File.ReadAllText("sample.text"));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                });
+                Task.Delay(500).Wait();
+                File.WriteAllText("sample.text", "Hello!");
+            }
 
             Console.WriteLine("엔터키를 누르면 종료합니다.");
             Console.ReadLine();
