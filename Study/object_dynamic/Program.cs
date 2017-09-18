8ecd2807506607f8d9d93256c583c0987c61f5cf
ksj <ksj@idis.co.kr>
2017-09-18 17:05:53 +0900

    object_dynamic

---
+++ Study/object_dynamic/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace object_dynamic
+{
+    class A
+    {
+        public void SayIt()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+
+    class B
+    {
+        public void SayIt()
+        {
+            Console.WriteLine("Welcome!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object[] ar = { new A(), new B() };
+            //foreach (object item in ar)
+            //{
+            //    if (item is A) ((A)item).SayIt();
+            //    else if (item is B) ((B)item).SayIt();
+            //    else throw new NotSupportedException();
+            //}
+
+
+            dynamic[] ar = { new A(), new B() };
+            foreach (dynamic item in ar)
+            {
+                item.SayIt();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
