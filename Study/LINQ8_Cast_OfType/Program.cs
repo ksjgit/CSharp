909e25d3bca84facc3654eb58b9c519effa1b380
ksj <ksj@idis.co.kr>
2017-09-18 15:24:54 +0900

    LINQ8_Cast_OfType

---
+++ Study/LINQ8_Cast_OfType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ8_Cast_OfType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] ar = { 1, 2, 3, "ABC" };
+
+            //Console.WriteLine(ar.Select(c => (int)c).Sum());
+
+            //var q = ar.Cast<int>();
+            //Console.WriteLine(q.Sum());
+
+            var q = ar.OfType<int>();
+            Console.WriteLine(q.Sum());
+
+            Console.ReadLine();
+        }
+    }
+}
